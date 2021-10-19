33f56b6314150cabe601b74bb98c348379a8f669
sherwood-codebender <sherwood654vn@gmail.com>
2021-10-19 20:39:39 +0700

    useState

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [name, setName] = useState('Mash')
+  const [session, setSession] = useState({ number: 6, title: 'state' })
+  const [current, setCurrent] = useState(true)
+
+  const onClickHandler = () => {
+    setName('Programming with Mash')
+    setSession({ number: 7, title: 'Style' })
+    setCurrent(false)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>This is session number {session.number} and about {session.title}</Text>
+      <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
+      <Button title='Update State' onPress={onClickHandler}></Button>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#ffffff',
+    fontSize: 20,
+    fontStyle: 'italic',
+    margin: 10,
+  },
 });
+
+export default App;
