70d1145cf4f84d150ee2ec75e47cd557f146ce2d
sherwood-codebender <sherwood654vn@gmail.com>
2021-10-30 23:23:56 +0700

    List, ScrollView, Refresh

--- App.js
+++ App.js
@@ -4,58 +4,63 @@ import {
   View,
   Text,
   Button,
+  ScrollViewBase,
+  ScrollView,
+  RefreshControl,
 } from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('Mash')
-  const [session, setSession] = useState({ number: 6, title: 'state' })
-  const [current, setCurrent] = useState(true)
-
-  const onClickHandler = () => {
-    setName('Programming with Mash')
+  const [Items, setItems] = useState([
+    {key:1, item: 'Item 1'},
+    {key:2, item: 'Item 2'},
+    {key:3, item: 'Item 3'},
+    {key:4, item: 'Item 4'},
+    {key:5, item: 'Item 5'},
+    {key:6, item: 'Item 6'},
+    {key:7, item: 'Item 7'},
+    {key:8, item: 'Item 8'},
+  ]);
+  
+  const [Refreshing, setRefreshing] = useState(false) 
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...Items, {key: 69, item: 'Item 69'}]);
+    setRefreshing(false);
   }
-
   return (
-    <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
-      </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}>2</Text>
-      </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>3</Text>
-      </View>
+      <ScrollView
+      style={styles.body}
+      refreshControl={
+        <RefreshControl refreshing = {Refreshing}
+        onRefresh={onRefresh}
+        colors={['#ff00ff']} />
+      }
+      >
+      {
+        Items.map((i) => {
+          return (
+            <View style={styles.item} key={i.key}>
+              <Text style={styles.text}>{i.item}</Text>
+            </View>
+          )
+        })
+      }
       
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'space-between',
   },
-  view1: {
-    
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
+  item: {
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
-  },
-  view2: {
-    
-    backgroundColor: '#f00fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    
-    backgroundColor: '#f00f',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
   },
 
   text: {
@@ -65,10 +70,6 @@ const styles = StyleSheet.create({
     margin: 10,
     textTransform: 'uppercase',
   },
-  button: {
-    width : 200,
-    height : 100,
-  }
 });
 
 export default App;
