2d88a53aa6af496d1aa3914301285a4c83819e04
sherwood-codebender <sherwood654vn@gmail.com>
2021-11-15 00:58:43 +0700

    stack-nav

--- App.js
+++ App.js
@@ -1,131 +1,66 @@
 import React, { useState } from 'react';
+import { NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import ScreenA from './ScreenA';
+import ScreenB from './ScreenB';
+
 import {
   StyleSheet,
   View,
   Text,
-  TextInputBase,
-  TextInput,
-  Button,
-  TouchableOpacity,
-  Pressable,
-  Alert,
-  ToastAndroid,
+  Pressable
 } from 'react-native';
 
 class App extends React.Component {
   
   constructor(props){
     super(props)
-    this.state = {
-      userInput: "",
-      submit: false,
-    }
-    this.handleChangeText = this.handleChangeText.bind(this);
-    this.onPressHandler = this.onPressHandler.bind(this);
   }
 
-  handleChangeText(e)  {
-    this.setState({
-      userInput: e
-    });
-  }
+  Stack = createStackNavigator()
 
-  onPressHandler() {
-    this.state.userInput.length > 3 ?
-     this.setState({
-      submit: !this.state.submit,
-    }) :
-    // Alert.alert("Waring", "More than 3 characters", [{text: 'OK'}])
-    ToastAndroid.showWithGravity("More than 3 characters", ToastAndroid.LONG, ToastAndroid.TOP)
-  }
 
-  render (){
 
+  render ()
+  {
     return (
-    <View style={styles.body}>
-        <Text style={styles.text}>
-          Please write your name:
-        </Text>
-        <TextInput 
-          style={styles.input}
-          placeholder='Account'
-          value={this.state.userInput}
-          onChangeText={this.handleChangeText}
-        />
 
-        <Pressable
-          onPress={this.onPressHandler}
-          style={ ({pressed}) => [
-            {backgroundColor: pressed ? 'green' : "blue"}, 
-            styles.button ]
-          }
-          android_ripple={{color: 'red'}}
+    <NavigationContainer>
+      <this.Stack.Navigator>
+
+        <this.Stack.Screen 
+          name="Screen_A"
+          component={ScreenA}
+          options={{
+            header: () => null
+          }}
         >
-          <Text style={styles.text}> 
-            {this.state.submit ? "Clear" : 'Submit'}
-          </Text>
-        </Pressable>
+        </this.Stack.Screen>
 
+        <this.Stack.Screen 
+          name="Screen_B"
+          component={ScreenB}
+        >
+        </this.Stack.Screen>
 
-        {
-          this.state.submit &&
-          <Text style={styles.text}>
-          Your name is: {this.state.userInput}
-          </Text>
-        
-        }
+      </this.Stack.Navigator>
+    </NavigationContainer>
 
-    </View>
-    )
-  }
+  )}
 }
 
-// const App = () => {
-  
-//   return ( 
-//     <View style={styles.body}>
-//       <Text style={styles.text}>
-//         Please write your name:
-//       </Text>
-//       <TextInput 
-//         style={styles.input}
-//         placeholder='Account'
-//       />
-//     </View>
-//   )
-// };
-
-const styles = StyleSheet.create({
-  body: {
+const styles=StyleSheet.create({
+  body:{
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
-  text: {
-    color: 'black',
+  text:{
     fontSize: 40,
-    fontStyle: 'italic',
-    margin: 10,
-    textTransform: 'uppercase',
-  },
-
-  input: {
-    width: 200,
-    borderWidth: 1,
-    borderColor: '#555',
-    borderRadius: 5,
-    textAlign: 'center',
-    fontSize: 20,
-    marginBottom: 10
-  },
-
-  button: {
-    
-    width: 150,
-    height: 50,
-    alignItems: "center"
+    fontWeight: 'bold',
+    margin: 10
   }
-});
+})
 
 export default App;
